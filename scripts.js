2c61dccef659c52c1acb84989f8c5c4e101dd1b7
Lenny Gitonga <lennygitonga07@gmail.com>
2026-02-23 01:32:04 +0300

    add ageinmonths

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+function ageInMonths(age){
+    return age * 12;
+}
+
 let form = document.querySelector('form');
 
 form.addEventListener('submit', function(e) {
@@ -8,6 +12,16 @@ form.addEventListener('submit', function(e) {
     localStorage.setItem('name', name);
     localStorage.setItem('age', age);
 
-    localStorage.getItem('name');
-    localStorage.getItem('age');
-});
+    let storedName = localStorage.getItem('name');
+    let storedAge = localStorage.getItem('age');
+
+document.querySelector('#greetings').textContent = `Hey there ${storedName}, Thank you for stopping over!`;
+
+if (storedAge < 18) {
+    document.querySelector('#adultContent').textContent = ` You are not old enough to access adult content.`;
+} else{
+    document.querySelector('#adultContent').textContent = ` You are old enough to access adult content.`;
+}
+
+document.querySelector('#ageInMonths').textContent = `Your age in months is ${ageInMonths(storedAge)}`;
+};
