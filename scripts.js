15e9de1a325bdbc1d1ff8c6da61c7e9177fd3f1e
Lenny Gitonga <lennygitonga07@gmail.com>
2026-02-23 07:06:06 +0300

    add quote

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
+//Calulating age in months
 function ageInMonths(age){
     return age * 12;
 }
-
+// Handling form submission and storing data in localStorage
 let form = document.querySelector('form');
 
 form.addEventListener('submit', function(e) {
@@ -14,14 +15,20 @@ form.addEventListener('submit', function(e) {
 
     let storedName = localStorage.getItem('name');
     let storedAge = localStorage.getItem('age');
-
+//Personalized greeting
 document.querySelector('#greetings').textContent = `Hey there ${storedName}, Thank you for stopping over!`;
-
+// Check if the user is old enough to access adult content
 if (storedAge < 18) {
     document.querySelector('#adultContent').textContent = ` You are not old enough to access adult content.`;
 } else{
     document.querySelector('#adultContent').textContent = ` You are old enough to access adult content.`;
 }
-
+//Display the user's age in months
 document.querySelector('#ageInMonths').textContent = `Your age in months is ${ageInMonths(storedAge)}`;
+
+//Inspirational quote
+let quote = 'The best way to predict the future is to create it.';
+for (let i = 0; i < 5; i++) {
+    document.querySelector('#quote').textContent += quote;
+}
 };
